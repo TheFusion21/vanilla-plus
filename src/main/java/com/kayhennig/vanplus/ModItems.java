24ade63364045d686a4d8a1386fefeffb068407e
Kay Hennig <KayHennig98@gmail.com>
2024-11-18 14:25:00 +0100

    Formatting

--- src/main/java/com/kayhennig/vanplus/ModItems.java
+++ src/main/java/com/kayhennig/vanplus/ModItems.java
@@ -18,48 +18,49 @@ import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-  // FoodComponents
-  public class ModFoodComponents {
-    public static final FoodComponent PEAR = new FoodComponent.Builder().snack().nutrition(1).saturationModifier(0.3F)
-        .build();
-  }
-
-  // Food
-  public static final Item PEAR = registerItem("pear", new Item(new Item.Settings().food(ModFoodComponents.PEAR)));
-
-  private static Item registerItem(String name, Item item) {
-    Identifier itemId = Identifier.of(Vanillaplus.MOD_ID, name);
-
-    Item registeredItem = Registry.register(Registries.ITEM, itemId, item);
-
-    return registeredItem;
-  }
-
-  private static void addItemToGroup(FabricItemGroupEntries entries) {
-    entries.add(PEAR);
-  }
-
-  private static void addItemToLootTable(RegistryKey<LootTable> table, LootTable.Builder builder,
-      LootTableSource source, RegistryWrapper.WrapperLookup lookup) {
-    if (source.isBuiltin() && (Blocks.OAK_LEAVES.getLootTableKey().equals(table) ||
-        Blocks.SPRUCE_LEAVES.getLootTableKey().equals(table) ||
-        Blocks.BIRCH_LEAVES.getLootTableKey().equals(table) ||
-        Blocks.JUNGLE_LEAVES.getLootTableKey().equals(table) ||
-        Blocks.ACACIA_LEAVES.getLootTableKey().equals(table) ||
-        Blocks.DARK_OAK_LEAVES.getLootTableKey().equals(table) ||
-        Blocks.MANGROVE_LEAVES.getLootTableKey().equals(table) ||
-        Blocks.CHERRY_LEAVES.getLootTableKey().equals(table))) {
-      LootPool.Builder poolBuilder = LootPool.builder()
-          .with(ItemEntry.builder(ModItems.PEAR).weight(1));
-
-      builder.pool(poolBuilder);
+    // FoodComponents
+    public class ModFoodComponents {
+        public static final FoodComponent PEAR = new FoodComponent.Builder().snack().nutrition(1)
+                .saturationModifier(0.3F)
+                .build();
     }
-  }
 
-  public static void registerItems() {
-    ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemToGroup);
-    ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(ModItems::addItemToGroup);
+    // Food
+    public static final Item PEAR = registerItem("pear", new Item(new Item.Settings().food(ModFoodComponents.PEAR)));
 
-    LootTableEvents.MODIFY.register(ModItems::addItemToLootTable);
-  }
+    private static Item registerItem(String name, Item item) {
+        Identifier itemId = Identifier.of(Vanillaplus.MOD_ID, name);
+
+        Item registeredItem = Registry.register(Registries.ITEM, itemId, item);
+
+        return registeredItem;
+    }
+
+    private static void addItemToGroup(FabricItemGroupEntries entries) {
+        entries.add(PEAR);
+    }
+
+    private static void addItemToLootTable(RegistryKey<LootTable> table, LootTable.Builder builder,
+            LootTableSource source, RegistryWrapper.WrapperLookup lookup) {
+        if (source.isBuiltin() && (Blocks.OAK_LEAVES.getLootTableKey().equals(table) ||
+                Blocks.SPRUCE_LEAVES.getLootTableKey().equals(table) ||
+                Blocks.BIRCH_LEAVES.getLootTableKey().equals(table) ||
+                Blocks.JUNGLE_LEAVES.getLootTableKey().equals(table) ||
+                Blocks.ACACIA_LEAVES.getLootTableKey().equals(table) ||
+                Blocks.DARK_OAK_LEAVES.getLootTableKey().equals(table) ||
+                Blocks.MANGROVE_LEAVES.getLootTableKey().equals(table) ||
+                Blocks.CHERRY_LEAVES.getLootTableKey().equals(table))) {
+            LootPool.Builder poolBuilder = LootPool.builder()
+                    .with(ItemEntry.builder(ModItems.PEAR).weight(1));
+
+            builder.pool(poolBuilder);
+        }
+    }
+
+    public static void registerItems() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemToGroup);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(ModItems::addItemToGroup);
+
+        LootTableEvents.MODIFY.register(ModItems::addItemToLootTable);
+    }
 }
